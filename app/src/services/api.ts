a3dc26ba23c86a1968c19f0af9e3d83ab091555e
Murilo <murigod8@gmail.com>
2026-01-08 19:40:33 -0300

    fix: fixed baseUrl on services

--- app/src/services/api.ts
+++ app/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 const api = axios.create({
-  baseURL: "http://192.168.3.108:3000",
+  baseURL: "https://bank-iaqy.onrender.com",
 });
 api.interceptors.request.use(async (config) => {
   const token = await AsyncStorage.getItem("@auth_token");

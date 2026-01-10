802d1dca98de3a3a9c4cacb71d4bfdc688250ac0
Murilo <murigod8@gmail.com>
2026-01-09 21:23:40 -0300

    fix: removed status bar from whole app

--- app/App.tsx
+++ app/App.tsx
@@ -1,17 +1,27 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthProvider } from "./src/providers/AuthProvider";
 import { TransferProvider } from "./src/providers/TransferProvider";
-
 import Routes from "./src/navigation/Routes";
+import { useEffect } from "react";
+import * as NavigationBar from "expo-navigation-bar";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
+  useEffect(() => {
+    NavigationBar.setVisibilityAsync("hidden");
+    NavigationBar.setBehaviorAsync("overlay-swipe");
+  }, []);
+
   return (
-    <AuthProvider>
-      <TransferProvider>
-        <NavigationContainer>
-          <Routes />
-        </NavigationContainer>
-      </TransferProvider>
-    </AuthProvider>
+    <>
+      <StatusBar hidden />
+      <AuthProvider>
+        <TransferProvider>
+          <NavigationContainer>
+            <Routes />
+          </NavigationContainer>
+        </TransferProvider>
+      </AuthProvider>
+    </>
   );
 }
